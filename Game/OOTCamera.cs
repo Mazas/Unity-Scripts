cee1a78576f4733e4c4fcf74347eec4a09374303
Tadas Ivanauskas <ivanauskas15@gmail.com>
2016-11-19 09:43:37 +0100

    Add files via upload

---
+++ Game/OOTCamera.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class OOTCamera : MonoBehaviour
+{
+    private GameObject player;
+    public float distance;
+    private float speed;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");    //get player object, player need to be tagged
+        speed=player.GetComponent<PlayerController>().speed;    //get speed from player script
+    }
+
+    void Update()
+    {
+        float dist = Vector3.Distance(player.transform.position, transform.position);   //get the distance between camera and player
+
+        Vector3 behind = player.transform.position - new Vector3(player.transform.forward.x * distance, player.transform.forward.y - 1.0f, player.transform.forward.z * distance); 
+        transform.LookAt(player.transform);
+        //transform.position = Vector3.MoveTowards(transform.position, behind, Time.deltaTime);
+        if (Input.GetKey(KeyCode.H)) //reset camera behind player
+        {
+            transform.position = Vector3.MoveTowards(transform.position,behind,distance);
+        }
+        if (dist < distance) //if player is close to camera stop camera movement
+        {
+            transform.position.Set(transform.position.x, transform.position.y, transform.position.z);
+            transform.LookAt(player.transform);
+        }
+        else if (dist > distance * 2) //if player is far away, move camera behind the player fast
+        {
+            transform.LookAt(player.transform);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, player.transform.position.y + 1.0f, player.transform.position.z - distance),speed*2*Time.deltaTime);
+        }
+        else if (dist < distance * 2 && dist > distance) //if player is in range, slowly moving camera towards player
+        {
+            transform.LookAt(player.transform);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, player.transform.position.y + 1.0f, player.transform.position.z - distance),Time.deltaTime);           
+        }
+    }
+    public bool IsCameraReversed(bool curr)
+    {
+        if (transform.rotation.y>-0.3&&transform.rotation.y<0.3&&!player.GetComponent<PlayerController>().IsMovementKeyPressed())
+        {
+            return false;
+        } else if (transform.rotation.y < -0.7&& !player.GetComponent<PlayerController>().IsMovementKeyPressed() || transform.rotation.y > 0.7&&!player.GetComponent<PlayerController>().IsMovementKeyPressed())
+        {
+            return true;
+        }
+        return curr;
+    }
+}
---
+++ Game/PlayerController.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody rb;
+    private bool isOnFloor = true;
+    private Animator anim;
+    private GameObject mainCamera;
+    private bool reverse;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    void FixedUpdate()
+    {
+        reverse = mainCamera.GetComponent<OOTCamera>().IsCameraReversed(reverse);
+        if (isOnFloor)
+        {
+            if (!reverse)
+            {
+                if ((Input.GetKey(KeyCode.W) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))) || (Input.GetKey(KeyCode.UpArrow) && !(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))))
+                {
+                    anim.SetBool("IsWalking", true);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 0), speed * Time.fixedDeltaTime);
+                    Go();
+                }
+                if ((Input.GetKey(KeyCode.S) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))) || (Input.GetKey(KeyCode.DownArrow) && !(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))))
+                {
+                    anim.SetBool("IsWalking", true);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 180, 0), speed * Time.fixedDeltaTime);
+                    Go();
+                }
+                if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                {
+                    if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+                    {
+                        anim.SetBool("IsWalking", true);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 45, 0), speed * Time.fixedDeltaTime);
+                        Go();
+                    }
+                    else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+                    {
+                        anim.SetBool("IsWalking", true);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 135, 0), speed * Time.fixedDeltaTime);
+                        Go();
+                    }
+                    else
+                    {
+                        anim.SetBool("IsWalking", true);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 90, 0), speed * Time.fixedDeltaTime);
+                        Go();
+                    }
+                }
+                if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                {
+                    if (Input.GetKey(KeyCode.W))
+                    {
+                        anim.SetBool("IsWalking", true);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, -45, 0), speed * Time.fixedDeltaTime);
+                        Go();
+                    }
+                    else if (Input.GetKey(KeyCode.S) | Input.GetKey(KeyCode.DownArrow))
+                    {
+                        anim.SetBool("IsWalking", true);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, -135, 0), speed * Time.fixedDeltaTime);
+                        Go();
+                    }
+                    else
+                    {
+                        anim.SetBool("IsWalking", true);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, -90, 0), speed * Time.fixedDeltaTime);
+                        Go();
+                    }
+                }
+            }
+            else
+            {
+                if ((Input.GetKey(KeyCode.W) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))) || (Input.GetKey(KeyCode.UpArrow) && !(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))))
+                {
+                    anim.SetBool("IsWalking", true);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 180, 0), speed * Time.fixedDeltaTime);
+                    Go();
+                }
+                if ((Input.GetKey(KeyCode.S) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))) || (Input.GetKey(KeyCode.DownArrow) && !(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))))
+                {
+                    anim.SetBool("IsWalking", true);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 0), speed * Time.fixedDeltaTime);
+                    Go();
+                }
+                if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                {
+                    if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+                    {
+                        anim.SetBool("IsWalking", true);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, -135, 0), speed * Time.fixedDeltaTime);
+                        Go();
+                    }
+                    else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+                    {
+                        anim.SetBool("IsWalking", true);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, -45, 0), speed * Time.fixedDeltaTime);
+                        Go();
+                    }
+                    else
+                    {
+                        anim.SetBool("IsWalking", true);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, -90, 0), speed * Time.fixedDeltaTime);
+                        Go();
+                    }
+                }
+                if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                {
+                    if (Input.GetKey(KeyCode.W))
+                    {
+                        anim.SetBool("IsWalking", true);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 135, 0), speed * Time.fixedDeltaTime);
+                        Go();
+                    }
+                    else if (Input.GetKey(KeyCode.S) | Input.GetKey(KeyCode.DownArrow))
+                    {
+                        anim.SetBool("IsWalking", true);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 45, 0), speed * Time.fixedDeltaTime);
+                        Go();
+                    }
+                    else
+                    {
+                        anim.SetBool("IsWalking", true);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 90, 0), speed * Time.fixedDeltaTime);
+                        Go();
+                    }
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Space) && isOnFloor)
+            {
+                rb.AddForce(Vector3.up * 200);
+                isOnFloor = false;
+            }
+            if (!IsMovementKeyPressed() && isOnFloor)
+            {
+                anim.SetBool("IsWalking", false);
+            }
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag.Equals("Floor"))
+        {
+            isOnFloor = true;
+        }
+        else return;
+    }
+   public bool IsMovementKeyPressed()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            return true;
+        }
+        return false;
+    }
+    void Go()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+
+    }
+}
