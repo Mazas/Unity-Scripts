60f6d319d6a16bfc3b7090a2d1ddd156f30fb93b
Tadas Ivanauskas <ivanauskas15@gmail.com>
2017-01-03 10:11:36 +0100

    Add files via upload

---
+++ Game/Tadas_Attack_Animation.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tadas_Attack_Animation : MonoBehaviour
+{
+
+    private Animator anim;
+    private CapsuleCollider weaponCollider;
+    private GameObject[] weapons = new GameObject[3];
+    private int currentWeapon = 0;
+    public bool q1complete;
+    public bool q2complete;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        weapons[0] = GameObject.Find("cutter01");
+        weapons[1] = GameObject.Find("sword_epic");
+        weapons[2] = GameObject.Find("dagger_epic");
+        foreach (GameObject weap in weapons)
+        {
+            weap.SetActive(false);
+        }
+        weapons[0].SetActive(true);
+        weaponCollider = weapons[0].GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            if (GetCurrentWeapon()!="dagger")
+            {
+                weaponCollider.enabled = true;
+                anim.SetBool("Attack", true);
+            }
+            else
+            {
+                weaponCollider.enabled = true;
+                anim.SetBool("Dagger_Attack", true);
+            }
+        }
+        else
+        {
+            weaponCollider.enabled = false;
+            anim.SetBool("Attack", false);
+            anim.SetBool("Dagger_Attack", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+
+            if (GetCurrentWeapon()=="cutter"&& q1complete)
+            {
+                currentWeapon = 1;
+                weapons[0].SetActive(false);
+                weapons[1].SetActive(true);
+                weaponCollider = weapons[1].GetComponent<CapsuleCollider>();
+            }
+            else if ((GetCurrentWeapon() == "sword" && q2complete) || (GetCurrentWeapon() == "cutter" && q2complete && !q1complete))
+            {
+                currentWeapon = 2;
+                weapons[0].SetActive(false);
+                weapons[1].SetActive(false);
+                weapons[2].SetActive(true);
+                weaponCollider = weapons[2].GetComponent<CapsuleCollider>();
+            }
+            else
+            {
+                currentWeapon = 0;
+                weapons[1].SetActive(false);
+                weapons[2].SetActive(false);
+                weapons[0].SetActive(true);
+                weaponCollider = weapons[0].GetComponent<CapsuleCollider>();
+            }
+        }
+    }
+    public void Die()
+    {
+        if (!anim.GetBool("Dead"))
+        {
+            anim.SetBool("Dead", true);
+        }
+    }
+    public string GetCurrentWeapon()
+    {
+        switch (weapons[currentWeapon].name){
+            case "dagger_epic":
+                return "dagger";
+            case "cutter01":
+                return "cutter";
+            case "sword_epic":
+                return "sword";
+            default:
+                return null;
+        }
+    }
+}
---
+++ Game/Tadas_PlayerController.cs
@@ -0,0 +1,234 @@
+﻿using UnityEngine;
+
+public class Tadas_PlayerController : MonoBehaviour
+{
+    public float speed = 1f;
+
+    private Rigidbody rb;
+    public bool isOnFloor = true;
+    private Animator anim;
+    private float h;
+    private T_LockOnCamera cameraScript;
+
+    private bool paused = false;
+    private GameObject pauseMenu;
+
+    private LockOn_Targeting target;
+
+    void Start()
+    {
+        target = GetComponent<LockOn_Targeting>();
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        pauseMenu = GameObject.FindGameObjectWithTag("Pause");
+        pauseMenu.SetActive(false);
+        cameraScript = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<T_LockOnCamera>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+    void FixedUpdate()
+    {
+        if (!target.getIsLockedOn())
+        {
+            if (isOnFloor)
+            {
+                if ((Input.GetKey(KeyCode.W) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))) || (Input.GetKey(KeyCode.UpArrow) && !(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))))
+                {
+                    Go();
+                }
+                if ((Input.GetKey(KeyCode.S) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))) || (Input.GetKey(KeyCode.DownArrow) && !(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))))
+                {
+                    GoBack();
+                }
+                if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                {
+                    if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+                    {
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, transform.rotation.eulerAngles.y + 90, 0), speed * Time.fixedDeltaTime);
+                        Go();
+                    }
+                    else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+                    {
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, transform.rotation.eulerAngles.y + -90, 0), (speed / 3 * 2) * Time.fixedDeltaTime);
+                        GoBack();
+                    }
+                    else if (cameraScript.targeting)
+                    {
+                        transform.Translate(Vector3.right * speed * Time.deltaTime, Space.Self);
+                        anim.SetTrigger("WalkRight");
+
+                    }
+                    else
+                    {
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, transform.rotation.eulerAngles.y + 90, 0), speed * Time.fixedDeltaTime);
+                    }
+                }
+                if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                {
+                    if (Input.GetKey(KeyCode.W))
+                    {
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, transform.rotation.eulerAngles.y + -90, 0), speed * Time.fixedDeltaTime);
+                        Go();
+                    }
+                    else if (Input.GetKey(KeyCode.S) | Input.GetKey(KeyCode.DownArrow))
+                    {
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, transform.rotation.eulerAngles.y + 90, 0), (speed / 3 * 2) * Time.fixedDeltaTime);
+                        GoBack();
+                    }
+                    else if (cameraScript.targeting)
+                    {
+                        transform.Translate(Vector3.left * speed * Time.deltaTime, Space.Self);
+                        anim.SetTrigger("WalkLeft");
+                    }
+                    else
+                    {
+                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, transform.rotation.eulerAngles.y + -90, 0), speed * Time.fixedDeltaTime);
+                    }
+                }
+                if (Input.GetKeyDown(KeyCode.Space) && isOnFloor)
+                {
+                    //rb.AddForce(Vector3.up * 150);
+                    // isOnFloor = false;
+                    anim.SetBool("Jump", true);
+                }
+
+                if (!IsMovementKeyPressed() && isOnFloor)
+                {
+                    rb.constraints = RigidbodyConstraints.FreezeAll;
+                    anim.SetBool("Walk_Back", false);
+                    anim.SetFloat("Turn", 0f);
+                    anim.SetFloat("Run", 0f);
+                    anim.SetBool("Dagger_Run", false);
+                    anim.SetBool("WalkLeft", false);
+                    anim.SetBool("WalkRight", false);
+                    anim.SetBool("CombatIdle", false);
+                    anim.SetBool("BattleForward", false);
+                }
+            }
+        }
+        if (target.getIsLockedOn() == true)
+        {
+            if (isOnFloor)
+            {
+                transform.LookAt(target.getSelectedTarget());
+                if (Input.GetKey(KeyCode.W) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) || Input.GetKey(KeyCode.UpArrow) && !(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)))
+                {
+                    target.WalkForward();
+                }
+                if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.UpArrow))
+                {
+                    {
+                        target.RotateLeftForward();
+                    }
+                   
+                }
+                if (Input.GetKey(KeyCode.A)&& !(Input.GetKey(KeyCode.W)||(Input.GetKey(KeyCode.S)))||Input.GetKey(KeyCode.LeftArrow)&&!(Input.GetKey(KeyCode.UpArrow)||Input.GetKey(KeyCode.DownArrow)))
+                {
+                    target.RotateLeft();
+                }
+                if(Input.GetKey(KeyCode.D)&&Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.RightArrow)&&Input.GetKey(KeyCode.UpArrow))
+                {
+                    target.RotateRightForward();
+                }
+                if ((Input.GetKey(KeyCode.D)&&!(Input.GetKey(KeyCode.W)||(Input.GetKey(KeyCode.S)))||Input.GetKey(KeyCode.RightArrow)&&!(Input.GetKey(KeyCode.UpArrow)||Input.GetKey(KeyCode.DownArrow))))
+                {
+                    target.RotateRight();
+                }
+                if ((Input.GetKey(KeyCode.S) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))) ||( Input.GetKey(KeyCode.DownArrow) && !(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))))
+                {
+                    target.WalkBackwards();
+                }
+                if ((Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S))||(Input.GetKey(KeyCode.LeftArrow)&&Input.GetKey(KeyCode.DownArrow))){
+                    target.RotateLeftBackwards();
+                }
+                if ((Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))|| (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.DownArrow)))
+                {
+                    target.RotateRightBackwards();
+                }
+            }
+            if (!IsMovementKeyPressed() && isOnFloor)
+            {
+                rb.constraints = RigidbodyConstraints.FreezeAll;
+                anim.SetBool("Walk_Back", false);
+                anim.SetFloat("Turn", 0f);
+                anim.SetFloat("Run", 0f);
+                anim.SetBool("Dagger_Run", false);
+                anim.SetBool("WalkLeft", false);
+                anim.SetBool("WalkRight", false);
+                anim.SetBool("BattleForward", false);
+                anim.SetBool("CombatIdle", true);
+            }
+
+        }
+        if (Input.GetKey(KeyCode.I))
+        {
+            target.DeselectTarget();
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag.Equals("Floor"))
+        {
+            isOnFloor = true;
+        }
+        else return;
+    }
+
+    public bool IsMovementKeyPressed()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void Go()
+    {
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        anim.SetBool("Walk_Back", false);
+        if (GetComponent<Tadas_Attack_Animation>().GetCurrentWeapon() == "dagger")
+        {
+            anim.SetBool("Dagger_Run", true);
+        }
+        else
+        {
+            anim.SetFloat("Run", 0.5f);
+        }
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+        h = Input.GetAxis("Horizontal");
+        anim.SetFloat("Turn", h / 3 * 2);
+    }
+
+    void GoBack()
+    {
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        anim.SetBool("Walk_Back", true);
+        transform.Translate(Vector3.back * (speed / 3 * 2) * Time.deltaTime, Space.Self);
+        h = Input.GetAxis("Horizontal");
+        anim.SetFloat("Turn", h / 3 * 2);
+    }
+
+    public void PauseGame()
+    {
+        if (!paused)
+        {
+            paused = true;
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+        }
+        else
+        {
+            paused = false;
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+        }
+    }
+}
